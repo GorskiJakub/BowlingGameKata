8e7cbf67dcfee946c9d5f81e2b76919aacbb396d
GorskiJakub <jakubg93@o2.pl>
2015-05-15 00:42:32 +0200

    test1 red

---
+++ src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package BowlingGameKata.BowlingGameKata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testGutterGame() {
+		Game game = new Game();
+		
+		for(int i=0; i<20; i++)
+			game.roll(0);
+		
+		assertEquals(0, game.score());
+	}
+
+}
