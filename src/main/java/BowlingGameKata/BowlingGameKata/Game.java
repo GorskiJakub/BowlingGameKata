94ace8effeb282f14c94502ef5d4df2ea336c275
GorskiJakub <jakubg93@o2.pl>
2015-05-15 01:17:23 +0200

    test3 green

--- src/main/java/BowlingGameKata/BowlingGameKata/Game.java
+++ src/main/java/BowlingGameKata/BowlingGameKata/Game.java
@@ -2,13 +2,27 @@ package BowlingGameKata.BowlingGameKata;
 
 public class Game {
 
-	private int score = 0;
+	private int[] rolls = new int[21];
+	private int currentRoll =0;
 
 	public void roll(int i) {
-		score += i;
+		rolls[currentRoll++] = i;
 	}
 
 	public Object score() {
+		int score =0;
+		int i = 0;
+		
+		for(int temp =0; temp <10; temp++){
+			if(rolls[i] + rolls[i+1] == 10){
+				score += 10 + rolls[i+2];
+				i +=2;
+			}
+			else{
+				score += rolls[i] + rolls[i+1];
+				i+=2;
+			}
+		}
 		return score ;
 	}
 }
