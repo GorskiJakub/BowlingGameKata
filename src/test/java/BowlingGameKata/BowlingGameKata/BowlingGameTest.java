3f8874602a2eda35599aab8d7db7c042a395440d
GorskiJakub <jakubg93@o2.pl>
2015-05-15 01:08:53 +0200

    test2 refactorization

--- src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
+++ src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
@@ -2,25 +2,30 @@ package BowlingGameKata.BowlingGameKata;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
+import org.junit.Before;
 
 public class BowlingGameTest {
 
+	
+	private Game game;
+	@Before
+	public void StartUp(){
+		game = new Game();
+	}
+	
+	private void rollMany(int n, int pins){
+		for(int i=0; i<n; i++)
+			game.roll(pins);
+	}
+	
 	@Test
 	public void testGutterGame() {
-		Game game = new Game();
-		
-		for(int i=0; i<20; i++)
-			game.roll(0);
-		
+		rollMany(20, 0);
 		assertEquals(0, game.score());
 	}
 	@Test
 	public void gameWithoutTwentyPoints(){
-		Game game = new Game();
-		for(int i=0; i<20; i++){
-			game.roll(1);
-		}
-		
+		rollMany(20,1);
 		assertEquals(20, game.score());
 	}
 
