47c17bd245d3672d400dc4f2e95cc2233c206235
GorskiJakub <jakubg93@o2.pl>
2015-05-15 00:55:09 +0200

    test2 red

--- src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
+++ src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package BowlingGameKata.BowlingGameKata;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class BowlingGameTest {
@@ -12,6 +13,15 @@ public class BowlingGameTest {
 			game.roll(0);
 		
 		assertEquals(0, game.score());
+	}
+	@Test
+	public void gameWithoutTwentyPoints(){
+		Game game = new Game();
+		for(int i=0; i<20; i++){
+			game.roll(1);
+		}
+		
+		assertEquals(20, game.score());
 	}
 
 }
