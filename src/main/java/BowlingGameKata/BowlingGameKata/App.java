6f40dd5dabd7415e97d4550b82e16a0728740690
GorskiJakub <jakubg93@o2.pl>
2015-05-14 20:26:03 +0200

    initial commit

---
+++ src/main/java/BowlingGameKata/BowlingGameKata/App.java
@@ -0,0 +1,13 @@
+package BowlingGameKata.BowlingGameKata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
