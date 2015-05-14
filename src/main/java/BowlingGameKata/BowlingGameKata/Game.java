b685f1d8f008e9dac3f4e594ab688de107ca7255
GorskiJakub <jakubg93@o2.pl>
2015-05-15 01:51:37 +0200

    test4 refactorization

--- src/main/java/BowlingGameKata/BowlingGameKata/Game.java
+++ src/main/java/BowlingGameKata/BowlingGameKata/Game.java
@@ -14,20 +14,36 @@ public class Game {
 		int index = 0;
 		
 		for(int temp =0; temp <10; temp++){
-			if(rolls[index] ==10){
-				score += 10 + rolls[index +1] + rolls[index +2];
+			if(isStrike(index)){
+				score += 10 + strikeBonus(index);
 				index++;
 			}
 			else if(isSpare(index)){
-				score += 10 + rolls[index+2];
+				score += 10 + spareBonus(index);
 				index +=2;
 			}
 			else{
-				score += rolls[index] + rolls[index+1];
+				score += sumOfBallsInFrame(index);
 				index+=2;
 			}
 		}
 		return score ;
+	}
+
+	private int spareBonus(int index) {
+		return rolls[index + 2];
+	}
+
+	private int sumOfBallsInFrame(int index) {
+		return rolls[index] + rolls[index + 1];
+	}
+
+	private int strikeBonus(int index) {
+		return rolls[index + 1] + rolls[index + 2];
+	}
+
+	private boolean isStrike(int index) {
+		return rolls[index] == 10;
 	}
 
 	private boolean isSpare(int index) {
