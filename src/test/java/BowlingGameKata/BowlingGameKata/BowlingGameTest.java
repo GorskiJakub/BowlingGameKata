41baaff94ffffd8e075b859dcdef05364d618e37
GorskiJakub <jakubg93@o2.pl>
2015-05-15 01:11:40 +0200

    test3 red

--- src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
+++ src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
@@ -28,5 +28,12 @@ public class BowlingGameTest {
 		rollMany(20,1);
 		assertEquals(20, game.score());
 	}
-
+	@Test
+	public void testOneSpare_returnScoreSixteen(){
+		game.roll(5);
+		game.roll(5);
+		game.roll(3);
+		rollMany(17,0);
+		assertEquals(16, game.score());
+	}
 }
