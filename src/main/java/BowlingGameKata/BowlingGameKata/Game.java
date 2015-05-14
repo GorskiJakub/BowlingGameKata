72ff03eabbb476c1bc2b6cf3df501daab748148e
GorskiJakub <jakubg93@o2.pl>
2015-05-15 00:47:42 +0200

    test1 green

---
+++ src/main/java/BowlingGameKata/BowlingGameKata/Game.java
@@ -0,0 +1,14 @@
+package BowlingGameKata.BowlingGameKata;
+
+public class Game {
+
+	public void roll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object score() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
--- src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
+++ src/test/java/BowlingGameKata/BowlingGameKata/BowlingGameTest.java
@@ -1,7 +1,5 @@
 package BowlingGameKata.BowlingGameKata;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class BowlingGameTest {
